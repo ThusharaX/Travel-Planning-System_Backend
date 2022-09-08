6e91e968ddea9be96f96ccf55ac7fcea564b3446
Sahan Punchihewa <sahannilupul18@gmail.com>
2022-09-08 20:00:13 +0530

    Please handle errors here using .catch - Bug fixed

--- src/api/controllers/TourPackage.controller.js
+++ src/api/controllers/TourPackage.controller.js
@@ -17,8 +17,13 @@ export const insertTourPackage = async (request, response, next) => {
 // Get all Tour Pacakges
 
 export const getAllTourPackages = async (request, response, next) => {
-	await TourPackageService.getAllTourPackages("users").then(async (data) => {
-		request.handleResponse.successRespond(response)(data);
-		next();
-	});
+	await TourPackageService.getAllTourPackages("users")
+		.then(async (data) => {
+			request.handleResponse.successRespond(response)(data);
+			next();
+		})
+		.catch((error) => {
+			request.handleResponse.errorRespond(response)(error.message);
+			next();
+		});
 };
