ca6acca9a2cb0edc25b02d1acfc103de77105762
Sahan Punchihewa <sahannilupul18@gmail.com>
2022-10-06 01:38:25 +0530

    Tour Guide Controller bug fixed

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -55,8 +55,8 @@ router.post("/hotel-owner/login", controller.loginHotelOwner);
 router.post("/hotel-owner/register", controller.registerHotelOwner);
 
 // Tour Guide endpoints
-router.post("/tour-guide/register", controller.registerTourGuide);
-router.post("/tour-guide/login", controller.loginTourGuide);
+router.post("/tour-guide/register/", controller.registerTourGuide);
+router.post("/tour-guide/login/", controller.loginTourGuide);
 router.get("/tour-guide/", controller.getAllTourGuides);
 router.get("/tour-guide/:id", controller.getOneTourGuide);
 router.put("/tour-guide/:id", controller.updateTourGuide);
